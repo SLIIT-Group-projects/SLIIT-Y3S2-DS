4280454180c1eacc5fef3b9afb88e047cd405f81
P A DAHAM KAUSHIKA LAKSHMAN <116593030+DKLakshman@users.noreply.github.com>
2025-04-29 00:31:20 +0530

    history page created

--- frontend/src/routes/OrderRoutes.jsx
+++ frontend/src/routes/OrderRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Navigate } from "react-router-dom";
 import ViewBasket from "../components/orderComponents/viewBaskets";
 import ViewBasketItems from "../components/orderComponents/viewBasketItems";
 import CheckoutRoute from "../components/orderComponents/checkoutPage";
+import OrderHistoryRoute from "../components/orderComponents/orderHistoryPage";
 
 const token = localStorage.getItem("token");
 const role = localStorage.getItem("role");
@@ -36,6 +37,17 @@ export default function DeliveryRoutes() {
         element={
           token && role === "customer" ? (
             <CheckoutRoute />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+
+      <Route
+        path="/orderHistory"
+        element={
+          token && role === "customer" ? (
+            <OrderHistoryRoute />
           ) : (
             <Navigate to="/login" />
           )
